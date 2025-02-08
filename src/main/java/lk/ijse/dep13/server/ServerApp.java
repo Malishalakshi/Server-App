27fa378c5f01e2ded026de5ad151275e52b592b5
Maleesha Lakshani <malishalakshaniyapa@gmail.com>
2025-02-08 16:34:52 +0530

    Read request

--- src/main/java/lk/ijse/dep13/server/ServerApp.java
+++ src/main/java/lk/ijse/dep13/server/ServerApp.java
@@ -1,6 +1,6 @@
 package lk.ijse.dep13.server;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,6 +11,36 @@ public class ServerApp {
         while (true) {
             Socket socket = serverSocket.accept();
             System.out.println("Socket connected from " + socket.getRemoteSocketAddress());
+            new Thread(() -> {
+                try {
+                    InputStream is = socket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String commandLine = br.readLine();
+                    if (commandLine == null) return;
+                    String[] commands = commandLine.split(" ");
+                    String command = commands[0];
+                    String resourcePath = commands[1];
+                    System.out.println(command + " " + resourcePath);
+
+                    String host = null;
+                    String line;
+                    while ((line = br.readLine()) != null && !line.isBlank()){
+                        String key = line.split(":")[0].strip();
+                        String value = line.substring( line.indexOf( ":" )+1 ).strip();
+                        if(key.equalsIgnoreCase( "host" )){
+                            host = value;
+                        }
+                    }
+                    System.out.println(command + " " + host);
+
+
+
+                } catch (IOException e) {
+                    throw new RuntimeException( e );
+                }
+            }).start();
 
         }
 
